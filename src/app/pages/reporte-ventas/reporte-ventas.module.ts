a469f507f0c9641ef70ffaa0a53512990c6c917e
cbaque <claudio.sys16@gmail.com>
2024-01-17 16:17:16 -0500

    reporte ui ok

--- src/app/pages/reporte-ventas/reporte-ventas.module.ts
+++ src/app/pages/reporte-ventas/reporte-ventas.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -15,8 +15,11 @@ import { SharedModule } from '../../shared/shared.module';
     FormsModule,
     IonicModule,
     ReporteVentasPageRoutingModule,
-    SharedModule
+    SharedModule,
   ],
-  declarations: [ReporteVentasPage]
+  declarations: [ReporteVentasPage],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA  
+  ]
 })
 export class ReporteVentasPageModule {}
